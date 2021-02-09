8b8973939e02a2583d5268cfed154b10b3aa1de3
Alnya <69311410+Alnya@users.noreply.github.com>
2021-02-09 14:19:11 +0900

    update

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# This repository is for web-site "another-spaden".
+# This repository is for web-site [https://another-spaden.github.io]("another-spaden").
 ---
-
-
+## Apps
+-[speed-judge](https://another-spaden.github.io/html/speed-judge.html)
+> _"speed-judge"_ is for adjusting speeds of characters of _"Another Eden"_.
+---
 _On "another-spaden", we are looking for contributors to build this website with us.
 Please send me direct-message if you're interested in this initiative._
--- js/speed-judge.js
+++ js/speed-judge.js
@@ -4,12 +4,20 @@ function selectSpeed() {
     const def_speed = selected_speed.options[index].value;
     const max_speed = Math.round(def_speed * 1.1 * 100) / 100;
     const min_speed = Math.round(def_speed * 0.9 * 100) / 100;
+    const s_speed = Math.floor((max_speed / 0.9) + 1);
+    const k_speed = Math.floor((min_speed / 1.1) - 1);
     const first_message = "判定した速度は、「" + def_speed + "」です。";
     const second_message = "最大値は、「" + max_speed + "」。";
     const third_message = "最小値は、「" + min_speed + "」。";
+    const forth_message = "速度" + def_speed + "に対する確定先攻ラインは、";
+    const fifth_message = "速度" + def_speed + "に対する確定後攻ラインは、";
     document.getElementById("first_message").textContent = first_message;
     document.getElementById("second_message").textContent = second_message;
     document.getElementById("third_message").textContent = third_message;
+    document.getElementById("forth_message").textContent = forth_message;
+    document.getElementById("s_speed").textContent = "「" + s_speed + "」。";
+    document.getElementById("fifth_message").textContent = fifth_message;
+    document.getElementById("k_speed").textContent = "「" + k_speed + "」。";
 }
 
 window.onload = function addOptions() {
